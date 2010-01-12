4fb2e88827adda83eee9d6a1711ab641b7a8e4c8
coreyoconnor <coreyoconnor@gmail.com>
2010-01-12 15:57:13 -0800

    Modify verify_eval_terminfo_caps for easier profiling
    Ignore-this: d72dbbbe49ed280b9140f30013be73ee
    
    darcs-hash:20100112235713-f0a0d-0e52694cd0a01a77c91c80d0b352574ebeac4a7a.gz

--- test/verify_eval_terminfo_caps.hs
+++ test/verify_eval_terminfo_caps.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE NamedFieldPuns #-}
 module Main where
@@ -6,6 +7,7 @@ import Data.Marshalling
 
 import Data.Terminfo.Eval 
 import Data.Terminfo.Parse
+import Control.DeepSeq
 
 import qualified System.Console.Terminfo as Terminfo
 
@@ -87,6 +89,7 @@ from_capname ti name = fromJust $ Terminfo.getCapability ti (Terminfo.tiGetStr n
 
 main = do
     run_test $ do
+        eval_buffer :: Ptr Word8 <- liftIO $ mallocBytes (1024 * 1024) -- Should be big enough for all termcaps ;-)
         forM_ terminals_of_interest $ \term_name -> do
             liftIO $ putStrLn $ "testing parsing of caps for terminal: " ++ term_name
             mti <- liftIO $ try $ Terminfo.setupTerm term_name
@@ -98,32 +101,33 @@ main = do
                         liftIO $ putStrLn $ "\tevaluating cap: " ++ cap_name
                         case Terminfo.getCapability ti (Terminfo.tiGetStr cap_name) of
                             Just cap_def -> do
-                                _ <- verify ( "\teval cap " ++ cap_name ++ " -> " ++ show cap_def )
-                                            ( verify_eval_cap cap_def )
+                                let parse_result = parse_cap_expression cap_def
+                                let test_name = "\teval cap " ++ cap_name ++ " -> " ++ show cap_def
+                                _ <- case parse_result of
+                                    Left error -> verify test_name ( liftResult $ failed { reason = "prase error " ++ show error } )
+                                    Right !cap_expr -> verify test_name ( verify_eval_cap eval_buffer cap_expr )
                                 return ()
                             Nothing      -> do
                                 return ()
         return ()
     return ()
 
-verify_eval_cap cap_string
-    = case parse_cap_expression cap_string of
-        Left error -> 
-            liftResult $ failed { reason = "parse error " ++ show error }
-        Right expr -> 
-            forAll (vector 9) $ \input_values -> 
-                let byte_count = cap_expression_required_bytes expr input_values
-                in liftIOResult $ do
-                    start_ptr :: Ptr Word8 <- mallocBytes (fromEnum byte_count)
-                    end_ptr <- serialize_cap_expression expr input_values start_ptr
-                    free start_ptr
-                    case end_ptr `minusPtr` start_ptr of
-                        count | count < 0        -> 
-                                    return $ failed { reason = "End pointer before start pointer." }
-                              | toEnum count > byte_count -> 
-                                    return $ failed { reason = "End pointer past end of buffer by " 
-                                                             ++ show (toEnum count - byte_count) 
-                                                    }
-                              | otherwise        -> 
-                                    return succeeded
+{-# NOINLINE verify_eval_cap #-}
+verify_eval_cap :: Ptr Word8 -> CapExpression -> Int -> Property
+verify_eval_cap eval_buffer expr !junk_int = do
+    forAll (vector 9) $ \input_values -> 
+        let !byte_count = cap_expression_required_bytes expr input_values
+        in liftIOResult $ do
+            let start_ptr :: Ptr Word8 = eval_buffer
+            forM_ [0..100] $ \i -> serialize_cap_expression expr input_values start_ptr
+            end_ptr <- serialize_cap_expression expr input_values start_ptr
+            case end_ptr `minusPtr` start_ptr of
+                count | count < 0        -> 
+                            return $ failed { reason = "End pointer before start pointer." }
+                      | toEnum count > byte_count -> 
+                            return $ failed { reason = "End pointer past end of buffer by " 
+                                                     ++ show (toEnum count - byte_count) 
+                                            }
+                      | otherwise        -> 
+                            return succeeded
 
