ad4b218b68e468a6c5df70895d6d9c2e58af0b6c
Corey O'Connor <corey@ooyala.com>
2013-09-01 14:13:55 -0700

    factor out more input classify data for easier reading by me.

--- src/Graphics/Vty/Input.hs
+++ src/Graphics/Vty/Input.hs
@@ -2,7 +2,10 @@
 -- Copyright 2009-2010 Corey O'Connor
 {-# OPTIONS_GHC -Wall #-}
 {-# LANGUAGE ForeignFunctionInterface #-}
-module Graphics.Vty.Input ( module Graphics.Vty.Input.Data
+module Graphics.Vty.Input ( Key(..)
+                          , Modifier(..)
+                          , Button(..)
+                          , Event(..)
                           , initTermInput
                           )
     where
@@ -43,8 +46,9 @@ initTermInput escDelay terminal = do
   inputChannel <- newChan
   hadInput <- newEmptyMVar
   oattr <- getTerminalAttributes stdInput
-  let nattr = foldl withoutMode oattr [StartStopOutput, KeyboardInterrupts,
-                                       EnableEcho, ProcessInput, ExtendedFunctions]
+  let nattr = foldl withoutMode oattr [ StartStopOutput, KeyboardInterrupts
+                                      , EnableEcho, ProcessInput, ExtendedFunctions
+                                      ]
   setTerminalAttributes stdInput nattr Immediately
   set_term_timing
   let inputToEventThread :: IO ()
@@ -125,64 +129,6 @@ initTermInput escDelay terminal = do
 
       caps_classify_table
         = map_to_legacy_table [(x,y) | (Just x,y) <- map (first (getCapability terminal . tiGetStr)) $ caps_table]
-
-      ansi_classify_table :: [[([Char], (Key, [Modifier]))]]
-      ansi_classify_table =
-         [ let k c s = ("\ESC["++c,(s,[])) in [ k "G" KNP5
-                                              , k "P" KPause
-                                              , k "A" KUp
-                                              , k "B" KDown
-                                              , k "C" KRight
-                                              , k "D" KLeft
-                                              , k "H" KHome
-                                              , k "F" KEnd
-                                              , k "E" KBegin
-                                              ],
-
-           -- Support for arrows and KHome/KEnd
-           [("\ESC[" ++ charCnt ++ show mc++c,(s,m))
-            | charCnt <- ["1;", ""], -- we can have a count or not
-            (m,mc) <- [([MShift],2::Int), ([MCtrl],5), ([MMeta],3),
-                       ([MShift, MCtrl],6), ([MShift, MMeta],4)], -- modifiers and their codes
-            (c,s) <- [("A", KUp), ("B", KDown), ("C", KRight), ("D", KLeft), ("H", KHome), ("F", KEnd)] -- directions and their codes
-           ],
-
-           let k n s = ("\ESC["++show n++"~",(s,[]))
-           in zipWith k [2::Int,3,5,6,1,4]
-                        [KIns,KDel,KPageUp,KPageDown,KHome,KEnd],
-
-           let k n s = ("\ESC["++show n++";5~",(s,[MCtrl]))
-           in zipWith k [2::Int,3,5,6,1,4]
-                        [KIns,KDel,KPageUp,KPageDown,KHome,KEnd],
-
-           -- Support for simple characters.
-           [ (x:[],(KASCII x,[])) | x <- map toEnum [0..255] ],
-
-           -- Support for function keys (should use terminfo)
-           [ ("\ESC[["++[toEnum(64+i)],(KFun i,[])) | i <- [1..5] ],
-           let f ff nrs m = [ ("\ESC["++show n++"~",(KFun (n-(nrs!!0)+ff), m)) | n <- nrs ] in
-           concat [ f 6 [17..21] [], f 11 [23,24] [], f 1 [25,26] [MShift], f 3 [28,29] [MShift], f 5 [31..34] [MShift] ],
-           [ ('\ESC':[x],(KASCII x,[MMeta])) | x <- '\ESC':'\t':[' ' .. '\DEL'] ],
-
-           -- Ctrl+Char
-           [ ([toEnum x],(KASCII y,[MCtrl]))
-              | (x,y) <- zip ([0..31]) ('@':['a'..'z']++['['..'_']),
-                y /= 'i' -- Resolve issue #3 where CTRL-i hides TAB.
-           ],
-
-           -- Ctrl+Meta+Char
-           [ ('\ESC':[toEnum x],(KASCII y,[MMeta,MCtrl])) | (x,y) <- zip [0..31] ('@':['a'..'z']++['['..'_']) ],
-
-           -- Special support
-           [ -- special support for ESC
-             ("\ESC",(KEsc,[])) , ("\ESC\ESC",(KEsc,[MMeta])),
-
-             -- Special support for backspace
-             ("\DEL",(KBS,[])), ("\ESC\DEL",(KBS,[MMeta])),
-
-             -- Special support for Enter
-             ("\ESC\^J",(KEnter,[MMeta])), ("\^J",(KEnter,[])) ]
-         ]
 
   eventThreadId <- forkIO $ inputToEventThread
   inputThreadId <- forkIO $ inputThread
--- src/Graphics/Vty/Input/Data.hs
+++ src/Graphics/Vty/Input/Data.hs
@@ -32,6 +32,7 @@ map_to_legacy_table = map f
     where f (s, EvKey k mods) = (s, (k, mods))
           f _                 = error "no mapping for mouse or resize events"
 
+-- | classify table directly generated from terminfo cap strings
 caps_table :: [(String, Event)]
 caps_table =
     [ ("khome", EvKey KHome     [])
@@ -45,3 +46,62 @@ caps_table =
     , ("kRIT",  EvKey KRight    [MShift])
     ]
 
+
+-- | classify table for ANSI terminals
+ansi_classify_table :: [[([Char], (Key, [Modifier]))]]
+ansi_classify_table =
+ [ let k c s = ("\ESC["++c,(s,[])) in [ k "G" KNP5
+                                      , k "P" KPause
+                                      , k "A" KUp
+                                      , k "B" KDown
+                                      , k "C" KRight
+                                      , k "D" KLeft
+                                      , k "H" KHome
+                                      , k "F" KEnd
+                                      , k "E" KBegin
+                                      ],
+
+   -- Support for arrows and KHome/KEnd
+   [("\ESC[" ++ charCnt ++ show mc++c,(s,m))
+    | charCnt <- ["1;", ""], -- we can have a count or not
+    (m,mc) <- [([MShift],2::Int), ([MCtrl],5), ([MMeta],3),
+               ([MShift, MCtrl],6), ([MShift, MMeta],4)], -- modifiers and their codes
+    (c,s) <- [("A", KUp), ("B", KDown), ("C", KRight), ("D", KLeft), ("H", KHome), ("F", KEnd)] -- directions and their codes
+   ],
+
+   let k n s = ("\ESC["++show n++"~",(s,[]))
+   in zipWith k [2::Int,3,5,6,1,4]
+                [KIns,KDel,KPageUp,KPageDown,KHome,KEnd],
+
+   let k n s = ("\ESC["++show n++";5~",(s,[MCtrl]))
+   in zipWith k [2::Int,3,5,6,1,4]
+                [KIns,KDel,KPageUp,KPageDown,KHome,KEnd],
+
+   -- Support for simple characters.
+   [ (x:[],(KASCII x,[])) | x <- map toEnum [0..255] ],
+
+   -- Support for function keys (should use terminfo)
+   [ ("\ESC[["++[toEnum(64+i)],(KFun i,[])) | i <- [1..5] ],
+   let f ff nrs m = [ ("\ESC["++show n++"~",(KFun (n-(nrs!!0)+ff), m)) | n <- nrs ] in
+   concat [ f 6 [17..21] [], f 11 [23,24] [], f 1 [25,26] [MShift], f 3 [28,29] [MShift], f 5 [31..34] [MShift] ],
+   [ ('\ESC':[x],(KASCII x,[MMeta])) | x <- '\ESC':'\t':[' ' .. '\DEL'] ],
+
+   -- Ctrl+Char
+   [ ([toEnum x],(KASCII y,[MCtrl]))
+      | (x,y) <- zip ([0..31]) ('@':['a'..'z']++['['..'_']),
+        y /= 'i' -- Resolve issue #3 where CTRL-i hides TAB.
+   ],
+
+   -- Ctrl+Meta+Char
+   [ ('\ESC':[toEnum x],(KASCII y,[MMeta,MCtrl])) | (x,y) <- zip [0..31] ('@':['a'..'z']++['['..'_']) ],
+
+   -- Special support
+   [ -- special support for ESC
+     ("\ESC",(KEsc,[])) , ("\ESC\ESC",(KEsc,[MMeta])),
+
+     -- Special support for backspace
+     ("\DEL",(KBS,[])), ("\ESC\DEL",(KBS,[MMeta])),
+
+     -- Special support for Enter
+     ("\ESC\^J",(KEnter,[MMeta])), ("\^J",(KEnter,[])) ]
+ ]
