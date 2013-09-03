fad66a4b49159a8b9138ca83511ebe8e3c44481e
Corey O'Connor <corey@ooyala.com>
2013-09-02 23:13:18 -0700

    refactor some constants out of ansi_classify_table for readability and testing

--- src/Graphics/Vty/Input/Data.hs
+++ src/Graphics/Vty/Input/Data.hs
@@ -46,62 +46,95 @@ caps_table =
     , ("kRIT",  EvKey KRight    [MShift])
     ]
 
+nav_keys_0 :: [([Char], (Key, [Modifier]))]
+nav_keys_0 =
+    [ k "G" KNP5
+    , k "P" KPause
+    , k "A" KUp
+    , k "B" KDown
+    , k "C" KRight
+    , k "D" KLeft
+    , k "H" KHome
+    , k "F" KEnd
+    , k "E" KBegin
+    ]
+    where k c s = ("\ESC["++c,(s,[]))
 
--- | classify table for ANSI terminals
-ansi_classify_table :: [[([Char], (Key, [Modifier]))]]
-ansi_classify_table =
- [ let k c s = ("\ESC["++c,(s,[])) in [ k "G" KNP5
-                                      , k "P" KPause
-                                      , k "A" KUp
-                                      , k "B" KDown
-                                      , k "C" KRight
-                                      , k "D" KLeft
-                                      , k "H" KHome
-                                      , k "F" KEnd
-                                      , k "E" KBegin
-                                      ],
-
-   -- Support for arrows and KHome/KEnd
+-- Support for arrows and KHome/KEnd
+nav_keys_1 :: [([Char], (Key, [Modifier]))]
+nav_keys_1 =
    [("\ESC[" ++ charCnt ++ show mc++c,(s,m))
     | charCnt <- ["1;", ""], -- we can have a count or not
     (m,mc) <- [([MShift],2::Int), ([MCtrl],5), ([MMeta],3),
                ([MShift, MCtrl],6), ([MShift, MMeta],4)], -- modifiers and their codes
     (c,s) <- [("A", KUp), ("B", KDown), ("C", KRight), ("D", KLeft), ("H", KHome), ("F", KEnd)] -- directions and their codes
-   ],
+   ]
 
-   let k n s = ("\ESC["++show n++"~",(s,[]))
-   in zipWith k [2::Int,3,5,6,1,4]
-                [KIns,KDel,KPageUp,KPageDown,KHome,KEnd],
+nav_keys_2 :: [([Char], (Key, [Modifier]))]
+nav_keys_2 =
+    let k n s = ("\ESC["++show n++"~",(s,[]))
+    in zipWith k [2::Int,3,5,6,1,4]
+                 [KIns,KDel,KPageUp,KPageDown,KHome,KEnd]
 
-   let k n s = ("\ESC["++show n++";5~",(s,[MCtrl]))
-   in zipWith k [2::Int,3,5,6,1,4]
-                [KIns,KDel,KPageUp,KPageDown,KHome,KEnd],
+nav_keys_3 :: [([Char], (Key, [Modifier]))]
+nav_keys_3 =
+    let k n s = ("\ESC["++show n++";5~",(s,[MCtrl]))
+    in zipWith k [2::Int,3,5,6,1,4]
+                 [KIns,KDel,KPageUp,KPageDown,KHome,KEnd]
 
-   -- Support for simple characters.
-   [ (x:[],(KASCII x,[])) | x <- map toEnum [0..255] ],
+-- Support for simple characters.
+simple_chars :: [([Char], (Key, [Modifier]))]
+simple_chars = [ (x:[],(KASCII x,[])) | x <- map toEnum [0..255] ]
 
-   -- Support for function keys (should use terminfo)
-   [ ("\ESC[["++[toEnum(64+i)],(KFun i,[])) | i <- [1..5] ],
-   let f ff nrs m = [ ("\ESC["++show n++"~",(KFun (n-(nrs!!0)+ff), m)) | n <- nrs ] in
-   concat [ f 6 [17..21] [], f 11 [23,24] [], f 1 [25,26] [MShift], f 3 [28,29] [MShift], f 5 [31..34] [MShift] ],
-   [ ('\ESC':[x],(KASCII x,[MMeta])) | x <- '\ESC':'\t':[' ' .. '\DEL'] ],
+-- Support for function keys (should use terminfo)
+function_keys_0 :: [([Char], (Key, [Modifier]))]
+function_keys_0 = [ ("\ESC[["++[toEnum(64+i)],(KFun i,[])) | i <- [1..5] ]
 
-   -- Ctrl+Char
-   [ ([toEnum x],(KASCII y,[MCtrl]))
-      | (x,y) <- zip ([0..31]) ('@':['a'..'z']++['['..'_']),
-        y /= 'i' -- Resolve issue #3 where CTRL-i hides TAB.
-   ],
+function_keys_1 :: [([Char], (Key, [Modifier]))]
+function_keys_1 =
+    let f ff nrs m = [ ("\ESC["++show n++"~",(KFun (n-(nrs!!0)+ff), m)) | n <- nrs ] in
+    concat [ f 6 [17..21] [], f 11 [23,24] [], f 1 [25,26] [MShift], f 3 [28,29] [MShift], f 5 [31..34] [MShift] ]
 
-   -- Ctrl+Meta+Char
-   [ ('\ESC':[toEnum x],(KASCII y,[MMeta,MCtrl])) | (x,y) <- zip [0..31] ('@':['a'..'z']++['['..'_']) ],
+function_keys_2 :: [([Char], (Key, [Modifier]))]
+function_keys_2 = [ ('\ESC':[x],(KASCII x,[MMeta])) | x <- '\ESC':'\t':[' ' .. '\DEL'] ]
 
-   -- Special support
-   [ -- special support for ESC
-     ("\ESC",(KEsc,[])) , ("\ESC\ESC",(KEsc,[MMeta])),
+-- Ctrl+Char
+ctrl_char_keys :: [([Char], (Key, [Modifier]))]
+ctrl_char_keys =
+    [ ([toEnum x],(KASCII y,[MCtrl]))
+    | (x,y) <- zip ([0..31]) ('@':['a'..'z']++['['..'_']),
+               y /= 'i' -- Resolve issue #3 where CTRL-i hides TAB.
+    ]
 
-     -- Special support for backspace
-     ("\DEL",(KBS,[])), ("\ESC\DEL",(KBS,[MMeta])),
+-- Ctrl+Meta+Char
+ctrl_meta_keys :: [([Char], (Key, [Modifier]))]
+ctrl_meta_keys =
+    [ ('\ESC':[toEnum x],(KASCII y,[MMeta,MCtrl])) | (x,y) <- zip [0..31] ('@':['a'..'z']++['['..'_']) ]
 
-     -- Special support for Enter
-     ("\ESC\^J",(KEnter,[MMeta])), ("\^J",(KEnter,[])) ]
- ]
+-- Special support
+special_support_keys :: [([Char], (Key, [Modifier]))]
+special_support_keys =
+    [ -- special support for ESC
+      ("\ESC",(KEsc,[])) , ("\ESC\ESC",(KEsc,[MMeta]))
+    -- Special support for backspace
+    , ("\DEL",(KBS,[])), ("\ESC\DEL",(KBS,[MMeta]))
+    -- Special support for Enter
+    , ("\ESC\^J",(KEnter,[MMeta])), ("\^J",(KEnter,[]))
+    ]
+
+-- | classify table for ANSI terminals
+ansi_classify_table :: [[([Char], (Key, [Modifier]))]]
+ansi_classify_table =
+    [ nav_keys_0
+    , nav_keys_1
+    , nav_keys_2
+    , nav_keys_3
+    , simple_chars
+    , function_keys_0
+    , function_keys_1
+    , function_keys_2
+    , ctrl_char_keys
+    , ctrl_meta_keys
+    , special_support_keys
+    ]
+
