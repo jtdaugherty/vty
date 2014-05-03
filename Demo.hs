e010924c61402928c719bba1c91a9a0db040fff6
Corey O'Connor <coreyoconnor@gmail.com>
2014-05-03 16:33:56 -0700

    add a demo program to vty distribution

---
+++ Demo.hs
@@ -0,0 +1,41 @@
+module Main where
+
+import Graphics.Vty
+
+import Control.Applicative
+import Control.Arrow
+import Control.Monad.RWS
+
+import Data.Default (def)
+import Data.Sequence (Seq, (<|) )
+import qualified Data.Sequence as Seq
+import Data.Foldable
+
+eventBufferSize = 1000
+
+type App = RWST Vty () (Seq String) IO
+
+main = do
+    vty <- mkVty def
+    _ <- execRWST (vtyInteract False) vty Seq.empty
+    shutdown vty
+
+vtyInteract :: Bool -> App ()
+vtyInteract shouldExit = do
+    updateDisplay
+    unless shouldExit $ handleNextEvent >>= vtyInteract
+
+updateDisplay :: App ()
+updateDisplay = do
+    let info = string defAttr "Press ESC to exit."
+    eventLog <- foldMap (string defAttr) <$> get
+    let pic = picForImage $ info <-> eventLog
+    vty <- ask
+    liftIO $ update vty pic
+
+handleNextEvent = ask >>= liftIO . nextEvent >>= handleEvent
+    where
+        handleEvent e               = do
+            modify $ (<|) (show e) >>> Seq.take eventBufferSize
+            return $ e == EvKey KEsc []
+
