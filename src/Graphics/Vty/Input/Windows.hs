0090758d0468156d7c55c42df9ef979d1fc78f43
Corey O'Connor <coreyoconnor@gmail.com>
2015-10-03 21:32:50 -0700

    Depend on forks of Win32 and ansi-terminal. These contain changes adding necessary Console functions to Win32. Incomplete, but enough to get started.
    TODO: Pull Request forks prior to merging this branch to master.

--- src/Graphics/Vty/Input/Windows.hs
+++ src/Graphics/Vty/Input/Windows.hs
@@ -4,5 +4,9 @@ import Graphics.Vty.Config
 import Graphics.Vty.Input.Events
 import Graphics.Vty.Input.Interface
 
+import System.Win32.Console
+import System.Win32.File
+import System.Win32.Types
+
 inputForConfig :: Config -> IO Input
 inputForConfig _ = undefined
