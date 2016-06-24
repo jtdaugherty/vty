bf57754328a76dbf200aaeff03abba072d44213d
Jonathan Daugherty <cygnus@foobox.com>
2016-06-24 16:44:06 -0700

    Classify: document Types module

--- src/Graphics/Vty/Input/Classify/Types.hs
+++ src/Graphics/Vty/Input/Classify/Types.hs
@@ -1,3 +1,5 @@
+-- | This module exports the input classification type to avoid import
+-- cycles between other modules that need this.
 module Graphics.Vty.Input.Classify.Types
     ( KClass(..)
     ) where
@@ -6,6 +8,11 @@ import Graphics.Vty.Input.Events
 
 data KClass
     = Valid Event [Char]
+    -- ^ A valid event was parsed. Any unused characters from the input
+    -- stream are also provided.
     | Invalid
+    -- ^ The input characters did not represent a valid event.
     | Prefix
+    -- ^ The input characters form the prefix of a valid event character
+    -- sequence.
     deriving(Show, Eq)
