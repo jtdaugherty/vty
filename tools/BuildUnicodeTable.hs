60c55bae3d780340b2174b3d5a93d420d4ffc9b2
Jonathan Daugherty <cygnus@foobox.com>
2020-02-27 15:25:30 -0800

    Add tools/BuildUnicodeTable.hs, a testing program for unicode table API

---
+++ tools/BuildUnicodeTable.hs
@@ -0,0 +1,26 @@
+module Main where
+
+import System.Exit (exitFailure)
+
+import Graphics.Vty.UnicodeWidthTable.Types
+import Graphics.Vty.UnicodeWidthTable.IO
+
+main :: IO ()
+main = do
+    let r1 = WidthTableRange 0 100 1
+        r2 = WidthTableRange 101 200 2
+        t = UnicodeWidthTable { unicodeWidthTableRanges = [r1, r2]
+                              }
+
+    writeUnicodeWidthTable "testfile.dat" t
+    result <- readUnicodeWidthTable "testfile.dat"
+
+    case result of
+        Left msg -> do
+            putStrLn $ "Error reading table: " <> msg
+            exitFailure
+        Right t2 -> do
+            putStrLn "Original table:"
+            print t
+            putStrLn "New table:"
+            print t2
