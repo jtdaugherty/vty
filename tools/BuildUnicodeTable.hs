0db1f719237085a137989585646360187223fc0d
Jonathan Daugherty <cygnus@foobox.com>
2020-02-27 16:40:56 -0800

    vty-build-table: use GetOpt to clean up and improve command line UI

--- tools/BuildUnicodeTable.hs
+++ tools/BuildUnicodeTable.hs
@@ -1,31 +1,82 @@
 module Main where
 
+import Control.Monad (when)
+import Data.Maybe (fromMaybe)
+import System.Environment (getArgs, getProgName)
 import System.Exit (exitFailure)
+import System.Console.GetOpt
 
+import Graphics.Vty.Config (terminalWidthTablePath)
 import Graphics.Vty.UnicodeWidthTable.Types
 import Graphics.Vty.UnicodeWidthTable.IO
 import Graphics.Vty.UnicodeWidthTable.Query
 
+data Arg = Help
+         | OutputPath String
+         deriving (Eq, Show)
+
+options :: Config -> [OptDescr Arg]
+options config =
+    [ Option "h" ["help"] (NoArg Help)
+      "This help output"
+    , Option "p" ["path"] (ReqArg OutputPath "PATH")
+      ("The output path to write to (default: " <> fromMaybe "<none>" (configOutputPath config) <> ")")
+    ]
+
+data Config =
+    Config { configOutputPath :: Maybe FilePath
+           }
+           deriving (Show)
+
+mkDefaultConfig :: IO Config
+mkDefaultConfig = do
+    Config <$> terminalWidthTablePath
+
+usage :: IO ()
+usage = do
+    config <- mkDefaultConfig
+    pn <- getProgName
+    putStrLn $ "Usage: " <> pn <> " [options]"
+    putStrLn ""
+    putStrLn "This tool queries the terminal on stdout to determine the widths"
+    putStrLn "of Unicode characters rendered to the terminal. The resulting data"
+    putStrLn "is written to a table at the specified output path for later"
+    putStrLn "loading by Vty-based applications."
+    putStrLn ""
+
+    putStrLn $ usageInfo pn (options config)
+
+configFromArgs :: [Arg] -> Config -> Config
+configFromArgs [] c =
+    c
+configFromArgs (Help:as) c =
+    configFromArgs as c
+configFromArgs (OutputPath p:as) c =
+    configFromArgs as (c { configOutputPath = Just p })
+
 main :: IO ()
 main = do
-    let r1 = WidthTableRange 0 100 1
-        r2 = WidthTableRange 101 200 2
-        t = UnicodeWidthTable { unicodeWidthTableRanges = [r1, r2]
-                              }
+    defConfig <- mkDefaultConfig
+    strArgs <- getArgs
+    let (args, unused, errors) = getOpt Permute (options defConfig) strArgs
 
-    writeUnicodeWidthTable "testfile.dat" t
-    result <- readUnicodeWidthTable "testfile.dat"
+    when (not $ null errors) $ do
+        mapM_ putStrLn errors
+        exitFailure
 
-    case result of
-        Left msg -> do
-            putStrLn $ "Error reading table: " <> msg
+    when ((not $ null unused) || (Help `elem` args)) $ do
+        usage
+        exitFailure
+
+    let config = configFromArgs args defConfig
+
+    outputPath <- case configOutputPath config of
+        Nothing -> do
+            putStrLn "Error: could not obtain terminal width table path"
             exitFailure
-        Right t2 -> do
-            putStrLn "Original table:"
-            print t
-            putStrLn "New table:"
-            print t2
+        Just path -> return path
 
+    putStrLn "Querying terminal:"
     builtTable <- buildUnicodeWidthTable
-    print builtTable
-    writeUnicodeWidthTable "terminal_widths.dat" builtTable
+    writeUnicodeWidthTable outputPath builtTable
+    putStrLn $ "\nOutput table written to " <> outputPath
