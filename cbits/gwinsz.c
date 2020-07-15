2fabd997b6726c5f1e57e213a63d498ad6f94ac2
Jonathan Daugherty <cygnus@foobox.com>
2020-07-15 13:50:23 -0700

    Whitespace

--- cbits/gwinsz.c
+++ cbits/gwinsz.c
@@ -1,11 +1,11 @@
 #include <sys/ioctl.h>
 
 unsigned long vty_c_get_window_size(int fd) {
-	struct winsize w;
-	if (ioctl (fd, TIOCGWINSZ, &w) >= 0)
-		return (w.ws_row << 16) + w.ws_col;
-	else
-		return 0x190050;
+    struct winsize w;
+    if (ioctl (fd, TIOCGWINSZ, &w) >= 0)
+        return (w.ws_row << 16) + w.ws_col;
+    else
+        return 0x190050;
 }
 
 void vty_c_set_window_size(int fd, unsigned long val) {
