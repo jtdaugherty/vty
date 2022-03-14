a9e45da79e717d1d2b2d5d026c24cacabb75e143
Jonathan Daugherty <cygnus@foobox.com>
2022-03-14 15:03:58 -0700

    Make EvResize fields strict now that EvResize is no longer used to handle signals

--- src/Graphics/Vty/Input/Events.hs
+++ src/Graphics/Vty/Input/Events.hs
@@ -51,14 +51,9 @@ data Event
     -- row. Some terminals report only that a button was released
     -- without specifying which one; in that case, Nothing is provided.
     -- Otherwise Just the button released is included in the event.
-    | EvResize ~Int ~Int
-    -- ^ If read from 'eventChannel' this is the size at the time of the
-    -- signal. If read from 'nextEvent' this is the size at the time the
-    -- event was processed by Vty. Typically these are the same, but if
-    -- somebody is resizing the terminal quickly they can be different.
-    --
-    -- Note: this is a lazy constructor to support bottom/error values
-    -- passing through the input events channel.
+    | EvResize Int Int
+    -- The terminal window was resized and the size is provided in the
+    -- integer fields (width, height).
     | EvPaste ByteString
     -- ^ A paste event occurs when a bracketed paste input sequence is
     -- received. For terminals that support bracketed paste mode, these
