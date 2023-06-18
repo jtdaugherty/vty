f89d045c9a5479c7ce95e2aeab350e67cb4daaa3
Jonathan Daugherty <cygnus@foobox.com>
2023-06-18 14:12:12 -0700

    Whitespace / haddock nits

--- src/Graphics/Vty/Input.hs
+++ src/Graphics/Vty/Input.hs
@@ -121,19 +121,19 @@ where
 import Graphics.Vty.Input.Events
 import Control.Concurrent.STM
 
-data Input = Input
-    { -- | Channel of events direct from input processing. Unlike
-      -- 'nextEvent' this will not refresh the display if the next event
-      -- is an 'EvResize'.
-      eventChannel  :: TChan InternalEvent
-      -- | Shuts down the input processing. As part of shutting down the
-      -- input, this should also restore the input state.
-    , shutdownInput :: IO ()
-      -- | Restore the terminal's input state to what it was prior
-      -- to configuring input for Vty. This should be done as part of
-      -- 'shutdownInput' but is exposed in case you need to access it
-      -- directly.
-    , restoreInputState :: IO ()
-      -- | Log the specified message.
-    , inputLogMsg :: String -> IO ()
-    }
+data Input =
+    Input { eventChannel :: TChan InternalEvent
+          -- ^ Channel of events direct from input processing. Unlike
+          -- 'nextEvent' this will not refresh the display if the next
+          -- event is an 'EvResize'.
+          , shutdownInput :: IO ()
+          -- ^ Shuts down the input processing. As part of shutting down
+          -- the input, this should also restore the input state.
+          , restoreInputState :: IO ()
+          -- ^ Restore the terminal's input state to what it was prior
+          -- to configuring input for Vty. This should be done as part
+          -- of 'shutdownInput' but is exposed in case you need to
+          -- access it directly.
+          , inputLogMsg :: String -> IO ()
+          -- ^ Log the specified message.
+          }
